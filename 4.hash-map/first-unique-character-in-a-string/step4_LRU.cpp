1906ab4b7fae26282350fa67612fc29f103c8151
Hurukawa2121 <shujifurukawa1213@gmail.com>
2024-12-24 01:09:00 +0900

    LRUライクの実装

---
+++ 4.hash-map/first-unique-character-in-a-string/step4_LRU.cpp
@@ -0,0 +1,35 @@
+#include <set>
+#include <string>
+#include <list>
+#include <map>
+
+class Solution {
+public:
+  int firstUniqChar(const std::string& s) {
+    std::list<char> unique_character_list;
+    std::map<char, std::list<char>::iterator> character_to_iterator;
+    std::set<char> duplicated_characters;
+
+    for (char current_character : s) {
+      if (duplicated_characters.contains(current_character)) {
+        continue;
+      }
+      if (character_to_iterator.contains(current_character)) {
+        unique_character_list.erase(character_to_iterator[current_character]);
+        character_to_iterator.erase(current_character);
+        duplicated_characters.insert(current_character);
+      } else {
+        unique_character_list.push_back(current_character);
+        auto last_iter = prev(unique_character_list.end());
+        character_to_iterator[current_character] = last_iter;
+      }
+    }
+
+    if (unique_character_list.empty()) {
+      return -1;
+    }
+    char first_unique_character = unique_character_list.front();
+    int first_unique_index = s.find(first_unique_character);
+    return first_unique_index;
+  }
+};
